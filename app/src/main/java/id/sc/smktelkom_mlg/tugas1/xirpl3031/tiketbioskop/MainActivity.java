25eaec78c4b3e65ab5af7324b2e921d9b306de80
SavinaRP <savputramadhani@gmail.com>
2016-09-11 08:22:43 +0700

    Memulai Awal - First Commit

---
+++ app/src/main/java/id/sc/smktelkom_mlg/tugas1/xirpl3031/tiketbioskop/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sc.smktelkom_mlg.tugas1.xirpl3031.tiketbioskop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/id/sc/smktelkom_mlg/tugas1/xirpl3031/tiketbioskop/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package id.sc.smktelkom_mlg.tugas1.xirpl3031.tiketbioskop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
