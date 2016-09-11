c84163809291c6d578974dc2c00c2f2ba89f8c89
SavinaRP <savputramadhani@gmail.com>
2016-09-11 12:02:25 +0700

    Second Commit

--- app/src/main/java/id/sc/smktelkom_mlg/tugas1/xirpl3031/tiketbioskop/MainActivity.java
+++ app/src/main/java/id/sc/smktelkom_mlg/tugas1/xirpl3031/tiketbioskop/MainActivity.java
@@ -2,12 +2,62 @@ package id.sc.smktelkom_mlg.tugas1.xirpl3031.tiketbioskop;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama, etEmail;
+    Button bOk;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etEmail = (EditText) findViewById(R.id.editTextEmail);
+        bOk = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doProcess();
+            }
+        });
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            String email = etEmail.getText().toString();
+            tvHasil.setText("Data Anda \n\n" +
+                    "Nama : " + nama + "\n" +
+                    "Email : " + email + "\n" +
+                    "\nTerima Kasih Telah Berkunjung!");
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String email = etEmail.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama Belum Anda Isi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama Minimal 3 Karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        return valid;
     }
 }
